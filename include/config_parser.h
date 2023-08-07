952ec624a50f4a6d3d25530835d5d7842b373cee
lopata1 <andrej.dusanic4@gmail.com>
2023-08-07 14:34:44 +0200

    Started working on config file

---
+++ include/config_parser.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <unordered_map>
+#include <string>
+
+std::unordered_map<std::string, std::string> ParseConfig(std::string& config_path);
---
+++ src/config_parser.cc
@@ -0,0 +1,3 @@
+#include "../include/config_parser.h"
+
+std::unordered_map<std::string, std::string> ParseConfig(std::string& config_path);
